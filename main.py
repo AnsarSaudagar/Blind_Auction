a101cb3bd52fd9cd8b6baeb1c131619090b37259
Ansar <ansarsaudagar40@gmail.com>
2022-08-06 16:02:43 +0530

    Separated the keys and values of dictionary in lists

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ print(logo)
 game_on = True
 
 dict_main = {}
+keys = []
+values = []
 
 while game_on:
     name = input("What is your name?:")
     bid = input("What is your bid?: $")
+    continue_bid = input("Are there any other bidders? Type 'yes or 'no'.\n")
     dict_main[name] = bid
-    print(dict_main)
+
+    if continue_bid == 'yes':
+        clear()
+    elif continue_bid == 'no':
+        for key, value in dict_main.items():
+            keys.append(key)
+            values.append(value)
+        winner_bid = values.max()
+        winner_bid_index = winner_bid.index()
+        print(winner_bid_index)
