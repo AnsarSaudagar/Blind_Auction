3e12fdae16d64ee678fa3828ea5843200dbb7a3a
Ansar <ansarsaudagar40@gmail.com>
2022-08-06 15:47:58 +0530

    Get the inputs and initialize the dictionary

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ from replit import clear
 from art import logo
 
 print(logo)
+
+game_on = True
+
+dict_main = {}
+
+while game_on:
+    name = input("What is your name?:")
+    bid = input("What is your bid?: $")
+    dict_main[name] = bid
+    print(dict_main)
